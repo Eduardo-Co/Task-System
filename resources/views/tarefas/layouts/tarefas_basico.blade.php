e6e43f866b13785b2168f4ff1e4cd6a499296be0
Eduardo-Co <121448598+Eduardo-Co@users.noreply.github.com>
2024-02-26 00:25:20 -0300

    v2

--- resources/views/tarefas/layouts/tarefas_basico.blade.php
+++ resources/views/tarefas/layouts/tarefas_basico.blade.php
@@ -3,8 +3,8 @@
     <head>
         <title>Super Gestão - @yield('titulo')</title>
         <meta charset="utf-8">
-        <link rel="stylesheet" href="{{ asset('css/tarefa_estilo.css') }}">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link rel="stylesheet" href="{{ asset('/css/tarefa_estilo.css') }}">
+       
         <style>
             body{
                 margin: 5px;
