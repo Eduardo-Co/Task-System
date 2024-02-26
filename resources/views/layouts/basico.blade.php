4e299f4904e96dccb9300b5ad01b8d24f22835d5
Eduardo-Co <121448598+Eduardo-Co@users.noreply.github.com>
2024-02-26 01:11:32 -0300

    Update basico.blade.php

--- resources/views/layouts/basico.blade.php
+++ resources/views/layouts/basico.blade.php
@@ -4,7 +4,7 @@
         <title>Super Gestão - @yield('titulo')</title>
         <meta charset="utf-8">
         <base href="/">
-        <link rel="stylesheet" href="/css/estilo_basico.css">
+        <link rel="stylesheet" href="{{ asset('/css/tarefa_estilo.css') }}">
        
         
         <style>
